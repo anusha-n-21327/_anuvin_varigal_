fc1671245c88078ebfceea0ec961bd99ca309718
[dyad] <itzanu.cyber@gmail.com>
2025-08-24 15:36:52 +0530

    [dyad] Added a new 'Whispers of Ink' theme - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider
       attribute="class"
-      defaultTheme="poetic"
+      defaultTheme="whispers-of-ink"
       enableSystem={false}
       storageKey="anu-theme"
     >
