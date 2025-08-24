63760a67b481ccb60990495a8d4dc8f027f93814
[dyad] <itzanu.cyber@gmail.com>
2025-08-24 15:17:17 +0530

    [dyad] Enhanced full-page animations with more icons and variety - wrote 1 file(s)

--- src/components/FloatingIcons.tsx
+++ src/components/FloatingIcons.tsx
@@ -1,16 +1,24 @@
-import { Feather, Pen, Book, Droplet } from "lucide-react";
+import { Feather, Pen, Book, Droplet, FileText } from "lucide-react";
 
 const iconClasses = "absolute text-primary/20 animate-float opacity-50 transition-transform duration-500 ease-in-out hover:scale-125 hover:opacity-70 hover:-translate-y-4 hover:rotate-12";
 
 export const FloatingIcons = () => {
   return (
     <div className="fixed inset-0 -z-10 pointer-events-none">
-      <Feather className={`${iconClasses} top-1/4 right-1/4 w-16 h-16`} />
-      <Pen className={`${iconClasses} top-1/2 left-1/4 w-12 h-12 [animation-delay:-1s]`} />
-      <Book className={`${iconClasses} bottom-1/4 right-1/3 w-14 h-14 [animation-delay:-2s]`} />
-      <Droplet className={`${iconClasses} bottom-1/3 left-1/3 w-10 h-10 [animation-delay:-3s]`} />
-      <Feather className={`${iconClasses} top-1/3 right-1/2 w-12 h-12 [animation-delay:-1.5s]`} />
-      <Pen className={`${iconClasses} bottom-1/2 left-1/5 w-10 h-10 [animation-delay:-2.5s]`} />
+      {/* Original Icons */}
+      <Feather className={`${iconClasses} top-[15%] right-[10%] w-16 h-16`} />
+      <Pen className={`${iconClasses} top-[50%] left-[15%] w-12 h-12 [animation-delay:-1s]`} />
+      <Book className={`${iconClasses} bottom-[20%] right-[20%] w-14 h-14 [animation-delay:-2s]`} />
+      <Droplet className={`${iconClasses} bottom-[30%] left-[30%] w-10 h-10 [animation-delay:-3s]`} />
+      <Feather className={`${iconClasses} top-[30%] right-[50%] w-12 h-12 [animation-delay:-1.5s]`} />
+      <Pen className={`${iconClasses} bottom-[55%] left-[5%] w-10 h-10 [animation-delay:-2.5s]`} />
+
+      {/* New Icons for more density */}
+      <FileText className={`${iconClasses} top-[10%] left-[10%] w-12 h-12 [animation-delay:-0.5s]`} />
+      <Book className={`${iconClasses} top-[70%] left-[45%] w-10 h-10 [animation-delay:-3.5s]`} />
+      <Feather className={`${iconClasses} bottom-[10%] left-[20%] w-14 h-14 [animation-delay:-4s]`} />
+      <Droplet className={`${iconClasses} top-[60%] right-[15%] w-12 h-12 [animation-delay:-4.5s]`} />
+      <Pen className={`${iconClasses} top-[85%] right-[40%] w-10 h-10 [animation-delay:-5s]`} />
     </div>
   );
 };
